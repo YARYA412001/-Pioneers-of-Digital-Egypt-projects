4bbc7e8e472b8de4d70b39b7011d689a27597034
Yahya Ahmed Ramadan Ramadan <156513535+YARYA412001@users.noreply.github.com>
2025-08-31 19:43:19 +0300

    Add files via upload

---
+++ DemoDay5/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoDay4.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
